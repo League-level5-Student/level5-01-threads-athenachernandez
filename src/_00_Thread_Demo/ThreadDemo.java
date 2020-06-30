12754fa52b505f92f17552899c44964d0096bc56
Athena Hernández <64385980+athenachernandez@users.noreply.github.com>
2020-06-29 19:44:13 -0700

    N

--- src/_00_Thread_Demo/ThreadDemo.java
+++ src/_00_Thread_Demo/ThreadDemo.java
@@ -9,17 +9,17 @@ public class ThreadDemo {
 			Robot tammy = new Robot(800, 700);
 			Robot sammy = new Robot(1200, 700);
 			
-			timmy.setSpeed(10);
-			tammy.setSpeed(10);
-			sammy.setSpeed(10);
-			
-			timmy.move(400);
-			tammy.move(400);
-			sammy.move(400);
-			
-			timmy.moveTo(400, 700);
-			tammy.moveTo(800, 700);
-			sammy.moveTo(1200, 700);
+//			timmy.setSpeed(10);
+//			tammy.setSpeed(10);
+//			sammy.setSpeed(10);
+//			
+//			timmy.move(400);
+//			tammy.move(400);
+//			sammy.move(400);
+//			
+//			timmy.moveTo(400, 700);
+//			tammy.moveTo(800, 700);
+//			sammy.moveTo(1200, 700);
 			
 			Thread r1 = new Thread(()->timmy.move(400));
 			Thread r2 = new Thread(()->tammy.move(400));
--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,48 @@
 package _01_Olympic_Rings;
 
+import java.util.ArrayList;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+	static ArrayList<Robot> robots = new ArrayList<Robot>();
 
+	// Make A Program that uses Threads and robots to draw the Olympic rings. One
+	// robot should draw one ring simultaneously with the other 4 robots.
+	public static void main(String[] args) {
+		Robot blue = new Robot(400, 700);
+		Robot yellow = new Robot(400, 700);
+		Robot black = new Robot(400, 700);
+		Robot green = new Robot(400, 700);
+		Robot red = new Robot(400, 700);
+
+		robots.add(blue);
+		robots.add(yellow);
+		robots.add(black);
+		robots.add(green);
+		robots.add(red);
+
+		blue.setSpeed(10);
+		yellow.setSpeed(10);
+		black.setSpeed(10);
+		green.setSpeed(10);
+		red.setSpeed(10);
+
+		Thread r6 = null;
+		Thread r6turn = null;
+		for (int j = 0; j < 360; j++) {
+			 r6 = new Thread(() -> blue.move(1));
+			 r6turn = new Thread(() -> blue.turn(1));
+		}
+
+//		Thread r1 = new Thread(() -> blue.move(400));
+//		Thread r2 = new Thread(() -> yellow.move(400));
+//		Thread r3 = new Thread(() -> black.move(400));
+//		Thread r4 = new Thread(() -> green.move(400));
+//		Thread r5 = new Thread(() -> red.move(400));
+
+		
+		r6.start();
+		r6turn.start();
+	}
 }
-
